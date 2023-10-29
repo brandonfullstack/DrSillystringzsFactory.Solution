f1f886678168ac55d2e514b5567688b3ae65a673
Brandon Wright <brandonfullstack@gmail.com>
2023-10-29 09:58:01 -0400

    Add models

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -1,11 +1,15 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Factory.Models
 {
   public class Engineer
   {
     public int EngineerId { get; set; }
+    [Required(ErrorMessage = "Please enter engineer's name to continue.")]
     public string Name { get; set; }
+    [Required(ErrorMessage = "Please enter engineer's description to continue.")]
+    public string Description { get; set; }
     public List<MachineEngineer> JoinEntities { get;}
   }
 }
--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -6,11 +6,20 @@ namespace Factory.Models
     public class Machine
     {
         public int MachineId { get; set; }
-        [Required(ErrorMessage = "The item's description can't be empty!")]
+        [Required(ErrorMessage = "The machine's description can't be empty!")]
         public string Description { get; set; }
-        [Range(1, int.MaxValue, ErrorMessage = "You must add your item to a category. Have you created a category yet?")]
-        public int EngineerId { get; set; }
-        public Engineer Engineer { get; set; }
-        public List<MachineEngineer> JoinEntities { get;}
+        [Required(ErrorMessage = "The machine's name can't be empty!")]
+        public string Name { get; set; }
+        public List<MachineEngineer> JoinEntities { get; }
     }
 }
+// public class Machine
+//     {
+//         public int MachineId { get; set; }
+//         [Required(ErrorMessage = "The Machine's description can't be empty!")]
+//         public string Description { get; set; }
+//         [Range(1, int.MaxValue, ErrorMessage = "You must add your engineer to a machine. Have you added a machine yet?")]
+//         public int EngineerId { get; set; }
+//         public Engineer Engineer { get; set; }
+//         public List<MachineEngineer> JoinEntities { get;}
+//     }

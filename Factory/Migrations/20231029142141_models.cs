7cd48f46be546bb169061bec2d03fb4ae1634bb9
Brandon Wright <brandonfullstack@gmail.com>
2023-10-29 10:25:17 -0400

    Add migrations

---
+++ Factory/Migrations/20231029142141_models.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Factory.Migrations
+{
+    public partial class models : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Machines_Engineers_EngineerId",
+                table: "Machines");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Machines_EngineerId",
+                table: "Machines");
+
+            migrationBuilder.DropColumn(
+                name: "EngineerId",
+                table: "Machines");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Machines",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Engineers",
+                keyColumn: "Name",
+                keyValue: null,
+                column: "Name",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Engineers",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Engineers",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Machines");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Engineers");
+
+            migrationBuilder.AddColumn<int>(
+                name: "EngineerId",
+                table: "Machines",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Engineers",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Machines_EngineerId",
+                table: "Machines",
+                column: "EngineerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Machines_Engineers_EngineerId",
+                table: "Machines",
+                column: "EngineerId",
+                principalTable: "Engineers",
+                principalColumn: "EngineerId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}

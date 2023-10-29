bb098f8a8833881ab681a542ed873528ee705dca
Brandon Wright <brandonfullstack@gmail.com>
2023-10-29 12:45:17 -0400

    Clean up code

--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -6,20 +6,10 @@ namespace Factory.Models
     public class Machine
     {
         public int MachineId { get; set; }
-        [Required(ErrorMessage = "The machine's description can't be empty!")]
+        [Required(ErrorMessage = "Please enter machine's description to continue.")]
         public string Description { get; set; }
-        [Required(ErrorMessage = "The machine's name can't be empty!")]
+        [Required(ErrorMessage = "Please enter machine's name to continue.")]
         public string Name { get; set; }
         public List<MachineEngineer> JoinEntities { get; }
     }
 }
-// public class Machine
-//     {
-//         public int MachineId { get; set; }
-//         [Required(ErrorMessage = "The Machine's description can't be empty!")]
-//         public string Description { get; set; }
-//         [Range(1, int.MaxValue, ErrorMessage = "You must add your engineer to a machine. Have you added a machine yet?")]
-//         public int EngineerId { get; set; }
-//         public Engineer Engineer { get; set; }
-//         public List<MachineEngineer> JoinEntities { get;}
-//     }
